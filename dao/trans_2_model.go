1638681eb57f568284a267c72a297b9d313a3041
Tianyi <chentyit@163.com>
2024-07-16 22:10:25 +0800

    修改模型转换逻辑

--- dao/trans_2_model.go
+++ dao/trans_2_model.go
@@ -5,18 +5,8 @@ import (
 	"github.com/Tianyi822/easy_blog_server/model/dto"
 )
 
-func TransArticleDao2Dto(ad *article.ArticleDao) *dto.ArticleDto {
-	return &dto.ArticleDto{
-		Aid:        ad.Aid,
-		Title:      ad.Title,
-		BriefIntro: ad.BriefIntro,
-		Url:        ad.Url,
-	}
-}
-
 func TransArticleDto2Dao(atDto *dto.ArticleDto) *article.ArticleDao {
 	return &article.ArticleDao{
-		Aid:        atDto.Aid,
 		Title:      atDto.Title,
 		BriefIntro: atDto.BriefIntro,
 		Url:        atDto.Url,
